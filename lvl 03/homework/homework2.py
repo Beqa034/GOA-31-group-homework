8d442b1ddc6a7f4141ac55e49d41f06c67fcc022
Beqa034 <BeqaGabro034@gmail.com>
2024-05-27 20:29:53 +0400

    first commit

---
+++ lvl 03/homework/homework2.py
@@ -0,0 +1,5 @@
+name = "Beqa"
+surname = "Gabroshvili"
+age = "13"
+hobby = "loves programing"
+print("I am " +name+" " +surname +" and i am " +age +" wlis" +" I "+ hobby)
---
+++ lvl 03/homework/homework5.py
@@ -0,0 +1,6 @@
+# from turtle import *
+print(20 - 20)
+print(30 + 30)
+print(10 * 10)
+print(200 // 5)
+print(3 ** 5)
